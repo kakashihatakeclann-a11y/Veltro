989c7d671a9dc607f3945bb71997a08fc9655dd0
kakashihatakeclann-a11y <kakashihatakeclann@gmail.com>
2026-06-03 19:51:06 +1200

    Update route.ts

--- app/api/gmail/route.ts
+++ app/api/gmail/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth"
-import { authOptions } from "../../auth/[...nextauth]/route"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest) {
